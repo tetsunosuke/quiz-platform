66a8dcf93881d6d00af2761ad2fc34f05a3af201
ITO Tetsunosuke <tetsunosuke.ito@photocreate.co.jp>
2020-12-10 13:56:53 +0900

    shuffleするモジュールを追加

---
+++ src/components/Module.js
@@ -0,0 +1,11 @@
+export function shuffle(base) {
+    console.log(base);
+    let array = [...base];
+
+    for (let i = array.length; 1 < i; i--) {
+        const k = Math.floor(Math.random() * i);
+        [array[k], array[i - 1]] = [array[i - 1], array[k]];
+    }
+
+    return array;
+}
